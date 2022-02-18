9df745ba2c5562d1c2e8c16b4cebfa2f2786674a
Dhruvawara <53032216+Dhruvawara@users.noreply.github.com>
2022-02-18 21:49:45 +0530

    Update r_1_30.py

--- from_DS_book/r_1_30.py
+++ from_DS_book/r_1_30.py
@@ -7,3 +7,15 @@ number by 2 before getting a value less than 2.
 
 def main():
     number = int(input("NUMBER : "))
+    count = 0
+    print(f'{number = }')
+    while number > 2:
+        number /= 2
+        if number > 2:
+            count += 1
+
+    print(f'{count = }')
+
+
+if __name__ == '__main__':
+    main()
